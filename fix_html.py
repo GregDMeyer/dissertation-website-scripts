b0dd5a6ea4e8920e3db9b2886b794ea7ed3a8900
Greg Kahanamoku-Meyer <gregory.meyer@gmail.com>
2024-01-05 17:03:34 -1000

    rewrite header and footer contents

--- fix_html.py
+++ fix_html.py
@@ -2,12 +2,9 @@
 This script takes the HTML produced by latexmlpost and makes some changes to it.
 '''
 
-# we use regex to parse HTML, despite the fact that it
-# is generally not recommended:
-# https://stackoverflow.com/questions/1732348/regex-match-open-tags-except-xhtml-self-contained-tags/1732454#1732454
-
 from sys import argv
-import re
+from bs4 import BeautifulSoup
+
 
 def replace(match):
     index = match.group(1)
@@ -16,10 +13,43 @@ def replace(match):
 
     return 'Chapter '+index+'</a'
 
+
 output_lines = []
 with open(argv[1]) as f:
-    for line in f:
-        output_lines.append(re.sub(r'<span class="ltx_tag ltx_tag_ref">([I0-9]*) </span>.*?</span></a', replace, line))
+    f_iter = iter(f)
+    for line in f_iter:
+        # skip the header and footer of index.html
+        if 'index.html' in argv[1] and any(s in line for s in ('<header', '<footer')):
+            line = next(f_iter)
+            while not any(s in line for s in ('</header', '</footer')):
+                line = next(f_iter)
+            continue
+
+        # shorten the names of chapters and parts in the header links
+        if line.startswith('<a ') and line.endswith('/a>\n'):
+            soup = BeautifulSoup(line, 'html.parser')
+            for a_node in soup.find_all('a'):
+                if 'rel' not in a_node.attrs:
+                    continue
+
+                if not(a_node['title'].startswith('Exploring the ') or a_node['title'].startswith('Part') or a_node['title'].startswith('Chapter')):
+                    continue
+
+                if a_node['title'].startswith('Part') or a_node['title'].startswith('Chapter'):
+                    a_node.span.span.decompose()
+                    new_content = ' '.join(a_node['title'].split(' ')[:2])
+                else:     # top level
+                    if 'prev' in a_node['rel']:
+                        a_node.decompose()
+                        continue
+                    new_content = 'Table of Contents'
+
+                a_node.span.string = new_content
+
+            line = soup.prettify(formatter='html5')
+
+        output_lines.append(line)
+
 
 with open(argv[1], 'w') as f:
     f.write(''.join(output_lines))
