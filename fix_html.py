2f8acc4326f662b3136736a50b8a1e5643bc715f
Greg Kahanamoku-Meyer <gregory.meyer@gmail.com>
2024-03-03 13:09:02 -1000

    fix broken headers after upgrading latexml

--- fix_html.py
+++ fix_html.py
@@ -14,42 +14,64 @@ def replace(match):
     return 'Chapter '+index+'</a'
 
 
-output_lines = []
-with open(argv[1]) as f:
-    f_iter = iter(f)
-    for line in f_iter:
-        # skip the header and footer of index.html
-        if 'index.html' in argv[1] and any(s in line for s in ('<header', '<footer')):
-            line = next(f_iter)
-            while not any(s in line for s in ('</header', '</footer')):
+def main():
+    output_lines = []
+    with open(argv[1]) as f:
+        f_iter = iter(f)
+        for line in f_iter:
+            # skip the header and footer of index.html
+            if 'index.html' in argv[1] and any(s in line for s in ('<header', '<footer')):
                 line = next(f_iter)
-            continue
+                while not any(s in line for s in ('</header', '</footer')):
+                    line = next(f_iter)
+                continue
 
-        # shorten the names of chapters and parts in the header links
-        if line.startswith('<a ') and line.endswith('/a>\n'):
-            soup = BeautifulSoup(line, 'html.parser')
-            for a_node in soup.find_all('a'):
-                if 'rel' not in a_node.attrs:
-                    continue
-
-                if not(a_node['title'].startswith('Exploring the ') or a_node['title'].startswith('Part') or a_node['title'].startswith('Chapter')):
-                    continue
-
-                if a_node['title'].startswith('Part') or a_node['title'].startswith('Chapter'):
-                    a_node.span.span.decompose()
-                    new_content = ' '.join(a_node['title'].split(' ')[:2])
-                else:     # top level
-                    if 'prev' in a_node['rel']:
-                        a_node.decompose()
+            # shorten the names of chapters and parts in the header links
+            if line.startswith('<a '):
+                while '</a>' not in line:
+                    line += next(f_iter)
+                soup = BeautifulSoup(line, 'html.parser')
+                for a_node in soup.find_all('a'):
+                    if 'rel' not in a_node.attrs:
                         continue
-                    new_content = 'Table of Contents'
 
-                a_node.span.string = new_content
+                    title = parse_href(a_node['href'])
+                    if title is None:
+                        continue
 
-            line = soup.prettify(formatter='html5')
+                    if 'Part' in title or 'Chapter' in title:
+                        a_node.span.span.decompose()
+                    else:     # top level
+                        if 'prev' in a_node['rel']:
+                            a_node.decompose()
+                            continue
 
-        output_lines.append(line)
+                    a_node.span.string = title
+
+                line = soup.prettify(formatter='html5')
+
+            output_lines.append(line)
 
 
-with open(argv[1], 'w') as f:
-    f.write(''.join(output_lines))
+    with open(argv[1], 'w') as f:
+        f.write(''.join(output_lines))
+
+
+def parse_href(val):
+    if val == 'index.html':
+        return 'Table of Contents'
+
+    num = val[2:][:-5]
+    if 'x' in num:
+        return None
+
+    if val.startswith('Pt'):
+        return 'Part ' + 'I'*int(num)
+    elif val.startswith('Ch'):
+        return f'Chapter {num}'
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    main()
